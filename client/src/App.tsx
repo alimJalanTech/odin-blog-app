3f17ae0643a2f962175782014052f8bb6db92334
Alim <52186295+alimalim77@users.noreply.github.com>
2023-05-08 15:05:11 +0530

    Commit: Minor Changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import "./App.css";
 
 interface Post {
   _id: string;
@@ -7,7 +8,9 @@ interface Post {
   content: string;
 }
 
-function App(): JSX.Element {
+interface AppProps {}
+
+const App: React.FC<AppProps> = (props: AppProps) => {
   const [posts, setPosts] = useState<Post[]>([]);
 
   useEffect(() => {
@@ -15,17 +18,16 @@ function App(): JSX.Element {
       setPosts(response.data);
     });
   }, []);
-
   return (
-    <div>
+    <div className="post-container">
       {posts.map((post) => (
-        <div key={post._id}>
+        <div key={post._id} className="post">
           <h2>{post.title}</h2>
           <p>{post.content}</p>
         </div>
       ))}
     </div>
   );
-}
+};
 
 export default App;
